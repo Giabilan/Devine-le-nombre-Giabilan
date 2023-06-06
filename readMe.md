aa412723de1206caf702a13995aac948e8a45029
Rajendran Giabilan <giabilan.r@gmail.com>
2023-06-06 19:35:34 +0200

    first commit

---
+++ readMe.md
@@ -0,0 +1,7 @@
+#Trouve_le_nombre
+
+Trouve le nombre entre 1 et 500.
+
+Nombre de vie illimité
+
+lien git pages : https://giabilan.github.io/Devine-le-nombre-Giabilan/
---
+++ script.js
@@ -0,0 +1,56 @@
+// Bouton "Go"
+let button = document.getElementById("button");
+// Input + bouton
+let form = document.getElementById("form");
+// Pararaphe, ID="message"
+let message = document.getElementById("message");
+// Link "Recommencer"
+let link = document.getElementById("link");
+// méthode pour avoir un nombre random
+const secretNumber = Math.floor(Math.random() * 501);
+
+// Nombre de vie illimité temporairement** 
+// nombre d'essai
+// let attempts = 10;
+
+// Ce bloc de code fonctionne que au click sur "button"
+button.onclick = function () {
+  const input = document.getElementById("input");
+  // Si l'input n'est pas vide
+  if (input.value != "") {
+    //   Si la valeur saisie est inférieur au nombres à trouver :
+    if (input.value < secretNumber) {
+      message.innerText = "Le nombre est plus grand que " + input.value + "";
+      message.style.color = "#999";
+    }
+    //   Si la valeur saisie est supérieur au nombres à trouver :
+    else if (input.value > secretNumber) {
+      message.innerText = "Le nombre est plus petit que " + input.value + "";
+      message.style.color = "#999";
+    }
+// Nombre de vie illimité temporairement** 
+//     // Si le nombre d'essai atteint 0 alors l'input + btn = none et link = flexc
+//     if (attempts == 0) {
+//       message.innerText =
+//         "T'es trop guez, il fallait trouvé " + secretNumber + " !";
+//       message.style.color = "red";
+//       form.style.display = "none";
+//       link.style.display = "flex";
+//     }
+//     attempts--;
+
+    // Si le nombre à été trouver alors l'input + btn = none et link = flex
+
+    if (input.value == secretNumber) {
+      message.innerText =
+        "Bravo vous avez trouvé le nombre qui est " + secretNumber + " !";
+      message.style.color = "green";
+      form.style.display = "none";
+      link.style.display = "flex";
+    }
+  } else {
+    // Sinon, si l'input est vide
+    message.innerText = "Le champ est vide !";
+    message.style.color = "red";
+  }
+};
